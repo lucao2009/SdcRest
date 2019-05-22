4c8546ccf24566be4f3225bdbca9cab4be055085
Walsan Jadson <walsanjl@gmail.com>
2019-05-22 16:42:51 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -26,6 +27,8 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@Column(unique = true)	//essa anotacao faz o banco de dados garantir que nao vai haver repeticao nesse campo
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
