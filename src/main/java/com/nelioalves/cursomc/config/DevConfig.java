e5676ca4d6f8e1b882e429e117fc8cf62fa923e2
Walsan Jadson <walsanjl@gmail.com>
2019-05-27 10:32:21 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/nelioalves/cursomc/config/DevConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/DevConfig.java
@@ -2,6 +2,7 @@ package com.nelioalves.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,14 +12,22 @@ import com.nelioalves.cursomc.services.DBService;
 //arquivo de configurações do profile de teste
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;	//estrategia de geracao do banco de dados
+	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
 		dbService.instantiateTestDatabase();
 		return true;
 	}
